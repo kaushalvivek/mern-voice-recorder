3e489d580a59761b084bd129f0a71d8d2fe01416
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2020-05-22 21:56:17 +0530

    models added

---
+++ backend/models/audio.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const audioSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  filepath: {
+    type: String,
+    required: true,
+  },
+  question: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true
+  }
+}, {
+  timestamps: true,
+});
+
+const Audio = mongoose.model('Audio', audioSchema);
+
+module.exports = Audio;
